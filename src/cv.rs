7d1c359a664da8b0fa5da0477d3c8365a05f38dd
Xin Liu <sam@secondstate.io>
2022-07-25 09:40:26 +0800

    [refactor](cv): Refactored code.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/cv.rs
+++ src/cv.rs
@@ -18,26 +18,35 @@ pub fn image_to_tensor(
     // Get an array of the pixel values
     let raw_u8_arr: &[u8] = &bgr_img.as_raw()[..];
 
-    let u8_f32_arr: Vec<u8> = match dtype {
-        Dtype::F32 => {
-            // Create an array to hold the f32 value of those pixels
-            let bytes_required = raw_u8_arr.len() * 4;
-            let mut u8_f32_arr: Vec<u8> = vec![0; bytes_required];
+    // Create an array to hold the f32 value of those pixels
+    let bytes_required = raw_u8_arr.len() * dtype.bytes();
+    let mut u8_arr: Vec<u8> = vec![0; bytes_required];
 
-            for i in 0..raw_u8_arr.len() {
-                // Read the number as a f32 and break it into u8 bytes
-                let u8_f32: f32 = raw_u8_arr[i] as f32;
-                let u8_bytes = u8_f32.to_ne_bytes();
+    for i in 0..raw_u8_arr.len() {
+        // Read the number as a f32 and break it into u8 bytes
+        let u8_f32: f32 = raw_u8_arr[i] as f32;
+        let u8_bytes = u8_f32.to_ne_bytes();
 
-                for j in 0..4 {
-                    u8_f32_arr[(i * 4) + j] = u8_bytes[j];
-                }
-            }
-
-            u8_f32_arr
+        for j in 0..dtype.bytes() {
+            u8_arr[(i * dtype.bytes()) + j] = u8_bytes[j];
         }
-        Dtype::U8 => raw_u8_arr.to_vec(),
-    };
+    }
 
-    Ok(u8_f32_arr)
+    Ok(u8_arr)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_image_to_tensor() {
+        let image_file = "/Volumes/Dev/secondstate/me/WasmEdge-WASINN-examples/openvino-road-segmentation-adas/rust/image/empty_road_mapillary.jpg";
+        let result = image_to_tensor(image_file, 512, 896, Dtype::F32);
+        assert!(result.is_ok());
+        let bytes = result.unwrap();
+
+        let result = std::fs::write("test_image_to_tensor.tensor", bytes.as_slice());
+        assert!(result.is_ok());
+    }
 }
